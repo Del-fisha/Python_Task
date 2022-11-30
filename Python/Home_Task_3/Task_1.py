498ffc514412fb2cfb0b680f376e87200facfedb
Алексей <del-fisha@yandex.ru>
2022-12-01 00:56:50 +0300

    Done 1,2

--- Python/Home_Task_3/Task_1.py
+++ Python/Home_Task_3/Task_1.py
@@ -1,3 +1,12 @@
 # Вводим с клавиатуры десятичное число. Необходимо
 # перевести его в шестнадцатеричную систему счисления.
 
+num = int(input("Введите число: "))
+string = ''
+system = '0123456789ABCDEF'
+
+while num > 0:
+    string = system[num % 16] + string
+    num = num // 16
+
+print(string)
